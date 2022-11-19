6786c1cd2ec9c3b2e7b2bb50ae421b9c1c204321
kylekrause3 <kkrause317@gmail.com>
2022-11-19 11:27:22 -0500

    aaaa

---
+++ OSRS Clone/Assets/Scripts/GlobalReferences.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalReferences : MonoBehaviour
+{
+    public Camera cam { get; set; }
+    public Transform hitSplats { get; set; }
+}
