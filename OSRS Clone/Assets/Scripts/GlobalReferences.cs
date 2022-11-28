ccf720d927299f47cdf7af86b982782025457189
kylekrause3 <kkrause317@gmail.com>
2022-11-28 14:01:13 -0500

    added a minimap

--- OSRS Clone/Assets/Scripts/GlobalReferences.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobalReferences : MonoBehaviour
-{
-    public Camera cam { get; set; }
-    public Transform hitSplats { get; set; }
-}
